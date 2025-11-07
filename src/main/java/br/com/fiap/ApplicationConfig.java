55ca610d3619fb1866f6d67662e4538596904b47
guilhermemacedomartins <guilhernemacedomartins@gmail.com>
2025-11-07 16:45:53 -0300

    atualizando application.properties e configurações do Swagger

--- src/main/java/br/com/fiap/ApplicationConfig.java
+++ src/main/java/br/com/fiap/ApplicationConfig.java
@@ -3,7 +3,7 @@ package br.com.fiap;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/")
+@ApplicationPath("/api")
 public class ApplicationConfig extends Application {
 }
 
